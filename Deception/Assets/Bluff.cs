37ec86fcb95c66f776ac66784bef11040cf9a628
CrazedLlamaz <36510487+CrazedLlamaz@users.noreply.github.com>
2019-08-25 22:55:25 -0500

    Begginning work on Example Script

---
+++ Deception/Assets/Bluff.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bluff : MonoBehaviour
+{
+    public bool honest;
+    GameObject bluffedCard;
+    GameObject bluffContextMenu;
+
+    public void BluffStart()
+    {
+        bluffContextMenu = gameObject.FindObjectWithTag("BluffContextUI");
+        bluffedCard = gameObject.FindObjectWithTag("BluffedCard");
+        honest = bluffedCard.GetComponent<CardScript>().bluffValue;
+
+        Time.timeScale = 0;
+
+
+    }
+}
