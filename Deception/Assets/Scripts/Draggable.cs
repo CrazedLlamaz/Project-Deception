41784b464e26a4d51cb76735a763ee207a4b6d4b
CrazedLlamaz <36510487+CrazedLlamaz@users.noreply.github.com>
2019-08-27 22:25:44 -0500

    drag and drop

---
+++ Deception/Assets/Scripts/Draggable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Transform parentToReturnTo = null;
+
+    public void OnBeginDrag(PointerEventData pointerEventData)
+    {
+        Debug.Log("On Begin Drag");
+
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+    }
+
+    public void OnDrag (PointerEventData pointerEventData)
+    {
+        Debug.Log("On Drag");
+
+        this.transform.position = pointerEventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData pointerEventData)
+    {
+        Debug.Log("On End Drag");
+        this.transform.SetParent(parentToReturnTo);
+    }
+}
